4bbaf39cc9905ce7c0e12edc74156908238e3133
unknown <jhullianchica@gmail.com>
2026-04-27 17:35:18 +0800

    Added error handling for missing file and invalid integers

--- programming exercises 1/programming_exercise_1.py
+++ programming exercises 1/programming_exercise_1.py
@@ -1,3 +1,22 @@
 class NumberCategorizer:
     def __init__(self, source_file: str = "numbers.txt"):
         self.source_file = source_file
+
+    def read_numbers_from_source(self) -> list[int]:
+
+        try:
+            with open(self.source_file, "r") as file:
+                number_list = [
+                    int(value.xtrip()) for value in file.readlines()
+                ]
+            return number_list
+
+        except FileNotFoundError:
+            print(f"File {self.source_file} not found.")
+            return []
+
+        except ValueError:
+            print("Error: File must contain only integers.")
+            return []
+
+
